e6b32c05e913ad148f6147eba3be6d727d690387
AndrewSmooth <gladkiy.a2004@gmail.com>
2023-09-14 09:38:42 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World111111!");
+        System.out.println("Hello Git!");
         //1
         System.out.println("---Task 1---");
         System.out.println(convert(5));
